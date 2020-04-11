a834ff90d67daaeea96ca7f41715589b16ca4bc2
Arthur Melkonyan <arthur@artmelkon.me>
2020-04-10 19:41:10 -0700

    update order.js

--- models/order.js
+++ models/order.js
@@ -1,5 +1,18 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.SchemaType({
-  
+  products: [{
+    productData: { type: Object, required: true },
+    quantity: { type: Number, required: true }
+  }],
+  user: {
+    name: { type: String, required: true },
+    userId: { 
+      type: mongoose.methods.OrderId,
+      ref: 'User',
+      required: true
+    }
+  }
 });
+
+module.export = mongoose.model('Order', orderSchema);
